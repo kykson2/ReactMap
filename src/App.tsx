73ae9e5295560ed55da7233f3ebeb1044f5419fd
김관우 <kimkwanwoo@gimgwan-uui-MacBookPro.local>
2021-11-07 01:51:44 +0900

    first commit & add kakao map

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Map from "./components/Map";
+import FindTeam from "./components/FindTeam";
+
+function App() {
+
+
+  return (
+    <div>
+      <Navbar />
+      <Map />
+      <FindTeam />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FindTeam.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function FindTeam() {
+    return <div>hi</div>
+}
+
+export default FindTeam;
